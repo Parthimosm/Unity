5c2f42c9721b910775b31f5232035303ebe55f28
Matthew <matthew.parthimos18@alumni.loyola.ca>
2016-09-04 12:50:33 -0400

    Add files via upload

---
+++ SpaceShooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyGO;
+
+    float maxSpawnRateInSeconds = 5f; 
+
+	// Use this for initialization
+	void Start()
+    {
+        Invoke ("SpawnEnemy", maxSpawnRateInSeconds);
+
+        //increase spawn rate every 30 secoonds 
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    //function to spawn an enemu
+    void SpawnEnemy()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        GameObject anEnemy = (GameObject)Instantiate(EnemyGO);
+        anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        //schedule when to spawn next enemy
+
+        ScheduleNextEnemySpawn(); 
+        
+    }
+
+    void ScheduleNextEnemySpawn()
+    {
+        float spawnInNSeconds;
+
+        if (maxSpawnRateInSeconds > 1f)
+        {
+            //picks a new number beetween 1 and maxSpawnRateInSecons
+            spawnInNSeconds = Random.Range(1, maxSpawnRateInSeconds);
+        }
+        else
+            spawnInNSeconds = 1f;
+
+        Invoke("SpawnEnemy", spawnInNSeconds); 
+
+    } 
+
+    //function to increase difficulty of game
+    void IncreaseSpawnRate()
+    {
+        if (maxSpawnRateInSeconds > 1f)
+            maxSpawnRateInSeconds--;
+        if (maxSpawnRateInSeconds == 1f)
+            CancelInvoke("IncreaseSpawnRate"); 
+
+    }
+
+}
