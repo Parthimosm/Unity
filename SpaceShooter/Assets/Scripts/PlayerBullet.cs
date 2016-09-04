18241048ff8d8d9ffa2daf4cb1bae473a36c5357
Matthew <matthew.parthimos18@alumni.loyola.ca>
2016-09-03 23:18:49 -0400

    Add files via upload

---
+++ SpaceShooter/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet: MonoBehaviour {
+
+    float speed; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 8f; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 position = transform.position;
+
+        //compute the bullets new position
+        position = new Vector2 (position.x, position.y  +speed * Time.deltaTime);
+
+        //update bullets position
+        transform.position = position;
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        //if bullet reaches end of screen then destroy
+        if(transform.position.y > max.y)
+        {
+            Destroy(gameObject); 
+        }
+	}
+}
